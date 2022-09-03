34d923aaca00b7046e39eda56ef3181210cfdc67
unknown <9446vamsi@gmail.com>
2022-09-03 21:46:35 +0530

    project1

---
+++ views/view.js
@@ -0,0 +1,27 @@
+// var mongoose = require('mongoose')
+
+// const UserdetailsSchema = new mongoose.Schema({
+//     id:{
+//         type:Number,
+//         required:true
+//     },
+//     name:{
+//         type:String,
+//         required:true
+//     },
+//     lang:{
+//         type:String,
+//         required:true
+
+//     },
+//     email:{
+//         type:String,
+//         required:true
+//     },
+//     age:{
+//         type:Number,
+//         required:true
+//     }
+
+// } );
+// module.exports=mongoose.model('project1',UserdetailsSchema)
